0a14529bbdd38021c6339b35f7f0d25dc80b84b2
TARZAN <mersinliarda123@gmail.com>
2026-05-01 23:07:05 +0300

    [Hız Kontrolü] Hız kontrolünün daha yumuşak geçişli olması için level sayısı artırıldı.

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ DOSYA_ADI = "AlphaBoostSound.wav"
 INTRO_BASLANGIC = 0.075
 LOOP_NOKTASI = 0.597     # Senin mükemmel aralığının başlangıcı
 LOOP_BITIS = 0.971       # Rüzgar sesinin asla girmediği bitiş noktası
-SES_SEVIYESI = 0.25
+SES_SEVIYESI = 0.3
 
 # --- KALİBRASYON VERİLERİNİ YÜKLE ---
 if not os.path.exists("config.json") or not os.path.exists("template_0.png"):
@@ -136,15 +136,23 @@ pygame.mixer.set_num_channels(8)
 
 # Sesleri ve Kanalı Yükle
 s_level1 = pygame.mixer.Sound("full_boost.wav")
-s_level2 = pygame.mixer.Sound("full_boost_mid1.wav")
-s_level3 = pygame.mixer.Sound("full_boost_mid2.wav")
-s_level4 = pygame.mixer.Sound("full_boost_high.wav")
+s_level2 = pygame.mixer.Sound("level_2.wav")
+s_level3 = pygame.mixer.Sound("level_3.wav")
+s_level4 = pygame.mixer.Sound("level_4.wav")
+s_level5 = pygame.mixer.Sound("level_5.wav")
+s_level6 = pygame.mixer.Sound("level_6.wav")
+s_level7 = pygame.mixer.Sound("level_7.wav")
+s_level8 = pygame.mixer.Sound("level_8.wav")
 
 # Ses Seviyelerini Uygula
 s_level1.set_volume(SES_SEVIYESI)
 s_level2.set_volume(SES_SEVIYESI)
 s_level3.set_volume(SES_SEVIYESI)
 s_level4.set_volume(SES_SEVIYESI)
+s_level5.set_volume(SES_SEVIYESI)
+s_level6.set_volume(SES_SEVIYESI)
+s_level7.set_volume(SES_SEVIYESI)
+s_level8.set_volume(SES_SEVIYESI)
 
 # --- FİZİK VE DURUM DEĞİŞKENLERİ ---
 is_mouse_down = False
@@ -167,15 +175,23 @@ def is_rl_active():
 def play_sound_from_start():
     ch = pygame.mixer.find_channel()
     if ch:
-        # Kademeli Hız Kontrolü (4 Adım - Çok Yumuşak Geçiş)
-        if estimated_speed < 550:
+        # Kademeli Hız Kontrolü (8 Adım - Ultra Yumuşak Geçiş)
+        if estimated_speed < 275:
             ch.play(s_level1)
-        elif estimated_speed < 1100:
+        elif estimated_speed < 550:
             ch.play(s_level2)
-        elif estimated_speed < 1650:
+        elif estimated_speed < 825:
             ch.play(s_level3)
-        else:
+        elif estimated_speed < 1100:
             ch.play(s_level4)
+        elif estimated_speed < 1375:
+            ch.play(s_level5)
+        elif estimated_speed < 1650:
+            ch.play(s_level6)
+        elif estimated_speed < 1925:
+            ch.play(s_level7)
+        else:
+            ch.play(s_level8)
         active_channels.append(ch)
 
 def stop_sound():
